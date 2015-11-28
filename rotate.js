19aadd06faa18455302e160394289ac6cf03ab95
Bill Neumann <Bill Neumann>
2015-11-28 12:06:27 -0500

    fixed issue

--- rotate.js
+++ rotate.js
@@ -3,26 +3,29 @@
  */
 
 
-var sheet = (function() {
-    var style=document.createElement('style');
-    style.appendChild(document.createTextNode(""));
-    document.head.appendChild(style);
-    return style.sheet;
-})();
 
-sheet.addRule('.ps-rototate-over','transform:rotate(-180deg )');
-sheet.addRule('.ps-rotate-out','transform:rotate( -360deg )');
+    var sheet = (function() {
+        var style = document.createElement('style');
+        style.appendChild(document.createTextNode(""));
+        document.head.appendChild(style);
+        return style.sheet;
+    })();
 
-var rotateEls = document.getElementsByClassName('rotate');
-for(var i=0; i< rotateEls.length; i++){
-    rotateEls[i].addEventListener('mouseover', function(){
-        this.className='ps-rotate-over';
+    sheet.addRule('.ps-rotate-over', 'transform:rotate( -180deg );-moz-transform:rotate( -180deg );-ms-transform:rotate( -180deg );-o-transform:rotate( -180deg );-webkit-transform: rotate( -180deg );transition:transform 550ms ease;-moz-transition:-moz-transform 550ms ease;-ms-transition:-ms-transform 550ms ease;-o-transition:-o-transform 550ms ease;-webkit-transition: -webkit-transform 550ms ease;');
+    sheet.addRule('.ps-rotate-out', 'transform:rotate( -360deg );-moz-transform:rotate( -360deg );-ms-transform:rotate( -360deg );-o-transform:rotate( -360deg );-webkit-transform: rotate( -360deg );transition:transform 550ms ease;-moz-transition:-moz-transform 550ms ease;-ms-transition:-ms-transform 550ms ease;-o-transition:-o-transform 550ms ease;-webkit-transition:-webkit-transform 550ms ease;')
 
-    }, false);
 
-    rotateEls[i].addEventListener( 'mouseout', function(){
-        var rotate=this;
-        rotate.className='ps-rotate-out';
-        window.setTimeout( function() {rotate.className=''},550)
-    }, false);
-}
+    var rotateEls = document.getElementsByClassName('rotate');
+    for(var i=0; i<rotateEls.length; i++) {
+        rotateEls[i].addEventListener( 'mouseover', function () {
+
+            this.className = 'ps-rotate-over';
+
+        }, false );
+
+        rotateEls[i].addEventListener( 'mouseout', function () {
+            var rotate = this;
+            rotate.className = 'ps-rotate-out';
+            window.setTimeout( function () { rotate.className = '' }, 550 );
+        }, false );
+    }
